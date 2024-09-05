6196c73d17bfead49dd555a069b370df3896860e
Azzam Maulana Dzakwan <azzamdzakwan088@gmail.com>
2024-09-05 09:07:50 +0700

    second commit

--- lib/pages/my_detail_page.dart
+++ lib/pages/my_detail_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gunung2/mobile/layout/mobile_detail_page.dart';
 import 'package:gunung2/models/mountain_models.dart';
 import 'package:gunung2/web/layout/detail_web_display.dart';
+import 'package:gunung2/web/layout/home_web_display.dart';
 
 class MyDetailPage extends StatelessWidget {
   final List<MountainModels> mountain;
@@ -8,8 +10,10 @@ class MyDetailPage extends StatelessWidget {
   const MyDetailPage({super.key, required this.mountain, required this.index});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: DetailWebDisplay(mountain: mountain, index: index),
-    );
+    if (MediaQuery.of(context).size.width >= 1110) {
+      return DetailWebDisplay(mountain: mountain, index: index);
+    } else {
+      return MobileDetailPage(mountain: mountain, index: index);
+    }
   }
 }
