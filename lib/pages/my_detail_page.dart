a3e46254e4b4e533547f04a0e9e6179e5c649f1c
Azzam Maulana Dzakwan <azzamdzakwan088@gmail.com>
2024-09-05 09:08:30 +0700

    clean dikit

--- lib/pages/my_detail_page.dart
+++ lib/pages/my_detail_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gunung2/mobile/layout/mobile_detail_page.dart';
 import 'package:gunung2/models/mountain_models.dart';
 import 'package:gunung2/web/layout/detail_web_display.dart';
-import 'package:gunung2/web/layout/home_web_display.dart';
+
 
 class MyDetailPage extends StatelessWidget {
   final List<MountainModels> mountain;
